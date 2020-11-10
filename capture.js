846fe894b63143ef6c26f4fb48191d2e179dd837
Logan Fuller <logankfuller@gmail.com>
2020-11-10 13:47:04 -0800

    Additional photo testing

--- capture.js
+++ capture.js
@@ -36,3 +36,20 @@ function handleError(error) {
 }
 
 navigator.mediaDevices.getUserMedia(constraints).then(handleSuccess).catch(handleError);
+
+function readURL() {
+    console.log("Blah" + this.files[0])
+    if (this.files && this.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            document.getElementById('blah').src =  e.target.result;
+        }
+
+        reader.readAsDataURL(this.files[0]);
+    }
+}
+
+const input = document.querySelector('input')
+
+input.addEventListener('change', readURL)
